1b2e11b1c9dc4380ded0aa8731553f929390c34c
Twinkle Joseph <twinkleljoseph@gmail.com>
2023-10-02 09:14:02 -0700

    display fields in table

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -2,11 +2,26 @@
   <div>
     <h2>Contact List</h2>
     <input :value="filter" @input="$emit('update:filter', $event.target.value)" placeholder="Filter by name" />
-    <ul>
-      <li v-for="contact in filteredContacts" :key="contact.id">
-        <strong>{{ contact.name }}</strong> - {{ contact.email }}
-      </li>
-    </ul>
+    <table class="contact-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Website</th>
+          <th>Company</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="contact in filteredContacts" :key="contact.id">
+          <td>{{ contact.name }}</td>
+          <td>{{ contact.email }}</td>
+          <td>{{ contact.phone }}</td>
+          <td><a :href="contact.website">{{ contact.website }}</a></td>
+          <td>{{ contact.company.name }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -23,3 +38,32 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+.contact-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-bottom: 1rem;
+}
+
+.contact-table th, .contact-table td {
+  border: 1px solid #ddd;
+  padding: 0.75rem;
+  text-align: left;
+}
+
+.contact-table th {
+  background-color: #f2f2f2;
+  font-weight: bold;
+}
+
+.contact-table td a {
+  color: #007BFF;
+  text-decoration: none;
+}
+
+.contact-table td a:hover {
+  text-decoration: underline;
+}
+</style>
